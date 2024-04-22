efac439fcc5225340bd7b0c382e284d5807dc6b0
mayukhs2904 <mayukhs@adobe.com>
2024-04-22 18:30:22 +0530

    article tags changes

--- blocks/article-tags/article-tags.js
+++ blocks/article-tags/article-tags.js
@@ -2,39 +2,36 @@ import { htmlToElement } from '../../scripts/scripts.js';
 import { getMetadata } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) {
-  const solution = getMetadata('coveo-solution');
-  const role = getMetadata('role');
-  const experienceLevel = getMetadata('level');
+  const solutions = getMetadata('coveo-solution').split(',');
+  const roles = getMetadata('role').split(',');
+  const experienceLevels = getMetadata('level').split(',');
 
   const [articleTagHeading] = [...block.children].map((row) => row.firstElementChild);
-
   block.innerHTML = '';
   block.classList.add('article-tags');
 
   const headerDiv = htmlToElement(`
-      <div class="article-tags-header">
-        <div class="article-tags-title">
-          ${articleTagHeading.innerHTML}
-        </div>
-        <div class="article-tags-view">
-        ${solution
-          .split(',')
-          .map((value) => `<div class="article-tags-name">${value.trim()}</div>`)
-          .join('')}
-        ${role
-          .split(',')
-          .map((value) => `<div class="article-tags-name">${value.trim()}</div>`)
-          .join('')}
-        ${experienceLevel
-          .split(',')
-          .map((value) => `<div class="article-tags-name">${value.trim()}</div>`)
-          .join('')}
-        </div>
+    <div class="article-tags-header">
+      <div class="article-tags-title">
+        ${articleTagHeading.innerHTML}
       </div>
-    `);
+      <div class="article-tags-view">
+        ${createTags(solutions)}
+        ${createTags(roles)}
+        ${createTags(experienceLevels)}
+      </div>
+    </div>
+  `);
 
   block.append(headerDiv);
+}
 
-  // const buildCardsShimmer = new BuildPlaceholder();
-  // buildCardsShimmer.add(block);
+function createTags(values) {
+  let tagsHTML = '';
+
+  values.forEach((value) => {
+    tagsHTML += `<div class="article-tags-name">${value.trim()}</div>`;
+  });
+
+  return tagsHTML;
 }
