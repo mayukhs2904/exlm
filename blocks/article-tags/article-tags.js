487388d11d9f6a115216bc33e3df69f55885dace
mayukhs2904 <mayukhs@adobe.com>
2024-04-22 17:57:55 +0530

    article tags code added

---
+++ blocks/article-tags/article-tags.js
@@ -0,0 +1,40 @@
+import { htmlToElement } from '../../scripts/scripts.js';
+import { getMetadata } from '../../scripts/lib-franklin.js';
+
+export default function decorate(block) {
+  const solution = getMetadata('coveo-solution');
+  const role = getMetadata('role');
+  const experienceLevel = getMetadata('level');
+
+  const [articleTagHeading] = [...block.children].map((row) => row.firstElementChild);
+
+  block.innerHTML = '';
+  block.classList.add('article-tags');
+
+  const headerDiv = htmlToElement(`
+      <div class="article-tags-header">
+        <div class="article-tags-title">
+          ${articleTagHeading.innerHTML}
+        </div>
+        <div class="article-tags-view">
+        ${solution
+          .split(',')
+          .map((value) => `<div class="article-tags-name">${value.trim()}</div>`)
+          .join('')}
+        ${role
+          .split(',')
+          .map((value) => `<div class="article-tags-name">${value.trim()}</div>`)
+          .join('')}
+        ${experienceLevel
+          .split(',')
+          .map((value) => `<div class="article-tags-name">${value.trim()}</div>`)
+          .join('')}
+        </div>
+      </div>
+    `);
+
+  block.append(headerDiv);
+
+  // const buildCardsShimmer = new BuildPlaceholder();
+  // buildCardsShimmer.add(block);
+}
