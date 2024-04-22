3ebf126a74f282ad07160845dc6c596bf2391232
mayukhs2904 <mayukhs@adobe.com>
2024-04-22 18:46:55 +0530

    empty block fix

--- blocks/article-tags/article-tags.js
+++ blocks/article-tags/article-tags.js
@@ -1,6 +1,16 @@
 import { htmlToElement } from '../../scripts/scripts.js';
 import { getMetadata } from '../../scripts/lib-franklin.js';
 
+function createTags(values) {
+    let tagsHTML = '';
+    if (values.length > 0) {
+        values.forEach((value) => {
+          tagsHTML += `<div class="article-tags-name">${value.trim()}</div>`;
+        });
+      }
+    return tagsHTML;
+  }
+
 export default function decorate(block) {
   const solutions = getMetadata('coveo-solution').split(',');
   const roles = getMetadata('role').split(',');
@@ -24,14 +34,4 @@ export default function decorate(block) {
   `);
 
   block.append(headerDiv);
-}
-
-function createTags(values) {
-  let tagsHTML = '';
-
-  values.forEach((value) => {
-    tagsHTML += `<div class="article-tags-name">${value.trim()}</div>`;
-  });
-
-  return tagsHTML;
 }
