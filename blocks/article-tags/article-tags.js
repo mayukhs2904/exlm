179527844504f1cf584d2ee830a5d74bda79bbda
mayukhs2904 <mayukhs@adobe.com>
2024-04-23 12:07:18 +0530

    article tag code

--- blocks/article-tags/article-tags.js
+++ blocks/article-tags/article-tags.js
@@ -1,38 +1,31 @@
 import { htmlToElement } from '../../scripts/scripts.js';
 import { getMetadata } from '../../scripts/lib-franklin.js';
 
-function createTags(values) {
-    let tagsHTML = '';
-        values.forEach((value) => {
-          tagsHTML += `<div class="article-tags-name">${value.trim()}</div>`;
-        });
-    return tagsHTML;
-  }
-
 export default function decorate(block) {
-    const solutions = getMetadata('coveo-solution')?.split(',') || [];
-    const roles = getMetadata('role')?.split(',') || [];
-    const experienceLevels = getMetadata('level')?.split(',') || [];
-  
-    // Check if any of the lists are empty or not present
-    if (solutions.length === 0 || roles.length === 0 || experienceLevels.length === 0) {
-      // If any list is empty, don't show the block
-      return;
-    }
+  const solutions = getMetadata('coveo-solution');
+  const roles = getMetadata('role');
+  const experienceLevels = getMetadata('level');
 
   const [articleTagHeading] = [...block.children].map((row) => row.firstElementChild);
+
   block.innerHTML = '';
   block.classList.add('article-tags');
 
   const headerDiv = htmlToElement(`
-    <div class="article-tags-header">
+    <div class="article-tags">
       <div class="article-tags-title">
         ${articleTagHeading.innerHTML}
       </div>
       <div class="article-tags-view">
-        ${createTags(solutions)}
-        ${createTags(roles)}
-        ${createTags(experienceLevels)}
+        ${[solutions, roles, experienceLevels]
+          .map((values) =>
+            values
+              .split(',')
+              .filter(Boolean)
+              .map((value) => `<div class="article-tags-name">${value.trim()}</div>`)
+              .join(''),
+          )
+          .join('')}
       </div>
     </div>
   `);
