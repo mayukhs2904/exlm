548035dbdfee50ca3c47167e01b6d6424b12cabc
mayukhs2904 <mayukhs@adobe.com>
2024-04-29 20:49:44 +0530

    id add

--- blocks/social-share-block/social-share-block.js
+++ blocks/social-share-block/social-share-block.js
@@ -1,47 +1,54 @@
 import { htmlToElement } from '../../scripts/scripts.js';
 import { decorateIcons } from '../../scripts/lib-franklin.js';
- 
+
 export default function decorate(block) {
   const socialDiv = block.firstElementChild;
   const socialNetworks = socialDiv.textContent.split(',').map((network) => network.trim());
- 
+
   block.textContent = '';
- 
-  const socialLinks = {
-    Facebook: `https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`,
-    'X (formerly Twitter)': `https://twitter.com/intent/tweet?url=${window.location.href}`,
-    LinkedIn: `https://www.linkedin.com/shareArticle?mini=true&url=${window.location.href}`,
-  };
-  function getObjectById(obj, ID) {
-    return obj.find((option) => option.id === ID);
-  }
+
   const socialIcons = [
     {
-    id: "Facebook",
-    value: 'Facebook',
-    icon: "fb-social-icon"
-  }
-];
- 
+      id: "Facebook",
+      value: 'Facebook',
+      icon: "fb-social-icon",
+      link: `https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`
+    },
+    {
+      id: "Twitter",
+      value: 'Twitter',
+      icon: "x-social-icon",
+      link: `https://twitter.com/intent/tweet?url=${window.location.href}`
+    },
+    {
+      id: "LinkedIn",
+      value: 'LinkedIn',
+      icon: "li-social-icon",
+      link: `https://www.linkedin.com/shareArticle?mini=true&url=${window.location.href}`
+    }
+  ];
+
+  const getObjectById = (obj, ID) => {
+    return obj.find((option) => option.id === ID);
+  };
+
   const headerDiv = htmlToElement(`
     <div class="social-share-block">
-    <div class="social-share-title">
-      ${'SHARE ON SOCIAL'}
-    </div>
-    <div class="social-share-view">
-      ${socialNetworks
-        .map(
-          (network) => `<a href="${socialLinks[network]}" target="_blank">
-      <div class="social-share-item">
-      <span class="icon icon-${getObjectById(socialIcons,network)}"></span></span><span class="social-share-name">${network}</span>
+      <div class="social-share-title">
+        SHARE ON SOCIAL
       </div>
-      </a>`,
-        )
-        .join('')}
-    </div>
+      <div class="social-share-view">
+        ${socialNetworks.map((network) => `
+          <a href="${getObjectById(socialIcons, network).link}" target="_blank">
+            <div class="social-share-item">
+              <span class="icon icon-${getObjectById(socialIcons, network).icon}"></span>
+              <span class="social-share-name">${network}</span>
+            </div>
+          </a>`).join('')}
+      </div>
     </div>
   `);
- 
+
   block.append(headerDiv);
   decorateIcons(block);
 }
