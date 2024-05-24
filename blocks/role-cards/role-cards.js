f28315697e08ebcd30c7c064412d600d6de7bd17
mayukhs2904 <mayukhs@adobe.com>
2024-05-24 14:11:05 +0530

    fix

--- blocks/role-cards/role-cards.js
+++ blocks/role-cards/role-cards.js
@@ -120,7 +120,7 @@ export default async function decorate(block) {
 
       if (isSignedIn) {
         const profileKey = checkbox.getAttribute('name');
-        const currentProfile = defaultProfileClient.getMergedProfile();
+        const currentProfile = defaultProfileClient.getProfile();
         const updatedRoles = currentProfile.role ? [...currentProfile.role] : [];
 
         console.log(profileKey,"profilekey")
