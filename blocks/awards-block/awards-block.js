db327832aebdc583a2f56f9c2c6ab6c96a91ba77
mayukhs2904 <mayukhs@adobe.com>
2024-06-20 12:19:46 +0530

    awards block changes

--- blocks/awards-block/awards-block.js
+++ blocks/awards-block/awards-block.js
@@ -1,26 +1,42 @@
-import AwardDataService from "../../scripts/data-service/award-data-service.js";
-import { getConfig } from '../../scripts/scripts.js';
 import { defaultProfileClient, isSignedInUser } from '../../scripts/auth/profile.js';
 
-const { awardUrl } = getConfig();
-
-async function getAwardsData() {
-    const dataSource = {
-        url: awardUrl
-    };
-    const awardDataService = new AwardDataService(dataSource);
-    const data = await awardDataService.fetchDataFromSource();
-    console.log("Data",data)
-}
-
 export default async function decorate(block) {
-  getAwardsData();
+  block.textContent = '';
   const isSignedIn = await isSignedInUser();
-  console.log(isSignedIn,"issigned")
+  let awardDetails = [];
+
+  const awardsBlockDiv = document.createRange().createContextualFragment(`
+    ${awardDetails.map((card)=>{
+      <><h1>{card.Title}</h1><span>{card.Timestamp}</span><div>{card.Description}</div></>
+    })}
+  `);
+
+  block.append(awardsBlockDiv);
+
   if (isSignedIn) {
     const profileData = await defaultProfileClient.getMergedProfile();
-    console.log(profileData,"profiledata")
     const skills = profileData?.skills;
-    console.log(skills,"skilss")
+    const awardedSkills = skills.filter(skill => skill.award===true);
+    awardDetails = awardedSkills
+      .map(skill => ({
+        originalTimestamp: skill.timestamp, // Keep the original timestamp for sorting
+        formattedTimestamp: formatTimestampToMonthYear(skill.timestamp),
+        title: skill.name,
+        description: skill.description
+      }))
+      .sort((a, b) => new Date(a.originalTimestamp) - new Date(b.originalTimestamp)) // Sort by original timestamp
+      .map(skill => ({
+        Timestamp: skill.formattedTimestamp,
+        Title: skill.title,
+        Description: skill.description
+      }));
+  }
+
+  function formatTimestampToMonthYear(timestamp) {
+    const date = new Date(timestamp);
+    const options = { year: 'numeric', month: 'short' }; // Using 'short' month format
+    const formattedMonth = date.toLocaleDateString(undefined, options).slice(0, 3).toUpperCase();
+    const year = date.getFullYear();
+    return `${formattedMonth} ${year}`;
   }
 }
