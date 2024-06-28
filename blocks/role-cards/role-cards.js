fad04b325d478740926de5add4ee72c443615dce
mayukhs2904 <mayukhs@adobe.com>
2024-06-28 13:38:41 +0530

    try

--- blocks/role-cards/role-cards.js
+++ blocks/role-cards/role-cards.js
@@ -59,6 +59,18 @@ export default async function decorate(block) {
   ];
 
   const roleCardsDiv = document.createRange().createContextualFragment(`
+      <div class="">
+        
+      </div>
+      <div class="industry-container">
+        <label for="cars">Choose a car:</label>
+        <select name="cars" id="cars">
+        <option value="volvo">Volvo</option>
+        <option value="saab">Saab</option>
+        <option value="opel">Opel</option>
+        <option value="audi">Audi</option>
+        </select>
+      </div>
       ${roleCardsData
         .map(
           (card, index) => `
