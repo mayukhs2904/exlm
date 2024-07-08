2c3377b06d22d6c31742ad8808affe5533e5b3d9
mayukhs2904 <mayukhs@adobe.com>
2024-07-08 16:03:08 +0530

    rola cards filter changes

--- blocks/role-and-industry/role-and-industry.js
+++ blocks/role-and-industry/role-and-industry.js
@@ -41,12 +41,13 @@ export default async function decorate(block) {
   
   if (isSignedIn) {
     const industryOptions = await fetchIndustryOptions();
-    updatedIndustryOptions = Array.isArray(industryOptions.data) ? industryOptions.data.map((industry) => ({
+    updatedIndustryOptions = industryOptions.data.map((industry) => ({
       ...industry,
       value: industry.Name,
       title: industry.Name,
-    })) : [];
-  }
+    }))
+  };
+  
   const [roleAndIndustryTitle, roleAndIndustryDescription] = block.querySelectorAll(':scope div > div');
 
   const roleCardsData = [
@@ -128,43 +129,13 @@ export default async function decorate(block) {
   block.textContent = '';
   block.append(roleAndIndustryDiv);
 
-  // const selectIndustryDropDown = new Dropdown(
-  //   block.querySelector('.industry-selection-dropdown'),
-  //   `${placeholders?.select || 'Select'}`,
-  //   updatedIndustryOptions,
-  // );
-  // if (isSignedIn) {
-  //   selectIndustryDropDown.handleOnChange((industrySelection) => {
-  //     defaultProfileClient.updateProfile('industryInterests', industrySelection, true);
-  //   });
-  // }
+  const selectIndustryDropDown = new Dropdown(
+    block.querySelector('.industry-selection-dropdown'),
+    `${placeholders?.select || 'Select'}`,
+    updatedIndustryOptions,
+  );
 
-  // if (isSignedIn) {
-  //   const profileData = await defaultProfileClient.getMergedProfile();
-  //   const role = profileData?.role;
-  //   const industryInterest = profileData?.industryInterests;
-
-  //   if (industryInterest) {
-  //     const selectedOption = industryInterest;
-  //     selectIndustryDropDown.updateDropdownValue(selectedOption);
-  //   }
-
-  //   role.forEach((el) => {
-  //     const checkBox = document.querySelector(`input[name="${el}"]`);
-  //     if (checkBox) {
-  //       checkBox.checked = true;
-  //       checkBox.closest('.role-cards-item').classList.toggle('role-cards-highlight', checkBox.checked);
-  //     }
-  //   });
-  // }
-
-  if (isSignedIn && updatedIndustryOptions.length > 0) {
-    const selectIndustryDropDown = new Dropdown(
-      block.querySelector('.industry-selection-dropdown'),
-      `${placeholders?.select || 'Select'}`,
-      updatedIndustryOptions,
-    );
-    
+  if (isSignedIn) {
     selectIndustryDropDown.handleOnChange((industrySelection) => {
       defaultProfileClient.updateProfile('industryInterests', industrySelection, true);
     });
