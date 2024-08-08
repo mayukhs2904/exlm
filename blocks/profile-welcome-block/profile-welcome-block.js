d65a870038c55737ad5d98b8e79157ce1b606bb1
mayukhs2904 <mayukhs@adobe.com>
2024-08-08 19:06:09 +0530

    try

--- blocks/profile-welcome-block/profile-welcome-block.js
+++ blocks/profile-welcome-block/profile-welcome-block.js
@@ -9,6 +9,23 @@ try {
   console.error('Error fetching placeholders:', err);
 }
 
+function decorateButtons(...buttons) {
+    return buttons
+      .map((div, index) => {
+        if (div) {
+          const a = div.querySelector('a');
+          if (a) {
+            a.classList.add('button');
+            if (index === 0) a.classList.add('secondary');
+            if (index === 1) a.classList.add('primary');
+            return a.outerHTML;
+          }
+        }
+        return '';
+      })
+      .join('');
+}
+
 const profileFlags = ['exlProfile', 'communityProfile'];
 const profileData = await fetchProfileData(profileFlags);
 
@@ -42,33 +59,32 @@ export default async function decorate(block) {
             <div class="profile-curated-card-eyebrowText">
             ${profileEyebrowText.textContent}
             </div>
-            <div class="profile-curated-card-profileHeading>
+            <div class="profile-curated-card-profileHeading">
             ${profileHeading.textContent}
             </div>
             <div class="profile-curated-card-profileDescription">
             ${profileDescription.textContent}
             </div>
         </div>
-
         <div class="profile-user-card">
             <div class="profile-user-card-left">
-            <div class="profile-image">
-            ${profilePicture}
-            </div>
-            <div class="profile-info">
-                <h3 class="profile-name">${adobeDisplayName}</h3>
-                <div class="profile-tag">${communityUserName}</div>
-                <div class="profile-org">${company}</div>
-            </div>
-            <div class="profile-title"><strong>TITLE:${communityUserTitle}</strong></div>
-            <div class="profile-location"><strong>LOCATION:${communityUserLocation}</strong></div>
+                <div class="profile-image">
+                ${profilePicture}
+                </div>
+                <div class="profile-info">
+                    <h3 class="profile-name">${adobeDisplayName}</h3>
+                    <div class="profile-tag">${communityUserName}</div>
+                    <div class="profile-org">${company}</div>
+                </div>
+                <div class="profile-title"><strong>TITLE:${communityUserTitle}</strong></div>
+                <div class="profile-location"><strong>LOCATION:${communityUserLocation}</strong></div>
             </div>
             <div class="profile-user-card-right">
-            <div class="profile-role"><strong>MY ROLE:${roles}</strong></div>
-            <div class="profile-industry"><strong>MY INDUSTRY:${industry}</strong></div>
-            <div class="profile-interests"><strong>MY INTERESTS:${interests}</strong></div>
-            <button>View profile settings</button>
-            </div>
+                <div class="profile-role"><strong>MY ROLE:${roles}</strong></div>
+                <div class="profile-industry"><strong>MY INDUSTRY:${industry}</strong></div>
+                <div class="profile-interests"><strong>MY INTERESTS:${interests}</strong></div>
+                 <div class='profile-cta'>${decorateButtons(profileCtaText)}</div>
+            </div>    
        </div>
     `);
     
