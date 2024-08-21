7d6fb89ebf7829357dfad58ade0137dfdf502fdc
mayukhs2904 <mayukhs@adobe.com>
2024-08-21 19:11:38 +0530

    try

--- blocks/profile-welcome-block/profile-welcome-block.js
+++ blocks/profile-welcome-block/profile-welcome-block.js
@@ -31,10 +31,12 @@ export default async function decorate(block) {
     profileHeading,
     profileDescription,
     profileCta,
+    profileCtaType,
     incompleteProfileText,
   ] = block.querySelectorAll(':scope div > div');
 
   console.log(profileCta,"profilecta")
+  console.log(profileCtaType,"type")
 
   const isSignedIn = await isSignedInUser();
   if (isSignedIn) {
